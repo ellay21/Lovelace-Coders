e1782f91bbb6c36f904ff4403f8dc3d02df6fd0e
Mesud melaku <149295529+ellay21@users.noreply.github.com>
2023-11-17 15:20:12 +0300

    Update Q4 xy.cpp

--- ETS0916_15_mesud_melaku/cpp/Q4 xy.cpp
+++ ETS0916_15_mesud_melaku/cpp/Q4 xy.cpp
@@ -1,15 +1,18 @@
 #include<iostream>
 #include<cmath>
- 
+
  using namespace std ;
+ 
  int main(){
  	double x,y,result;
- 	cout<<"please, Enter the base of the function: ";
- 	cin>> x;
- 	cout<<"please, Enter the exponent: ";
- 	cin>> y;
+ 	cout << "please, Enter the base of the function: ";
+ 	cin >> x;
+  
+ 	cout << "please, Enter the exponent: ";
+ 	cin >> y;
  	result = pow(x,y);
- 	cout<<"The reult is "<<result<<"." ;
- 	
+  
+ 	cout << "The result is "<< result << "." ;
+
  	return 0 ;
 }
