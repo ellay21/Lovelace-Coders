de186823d7aa49ba0f5138914d669df58234a51c
Mesud melaku <149295529+ellay21@users.noreply.github.com>
2023-11-17 14:32:34 +0300

    Update Q1 bmi.cpp

--- ETS0916_15_mesud_melaku/cpp/Q1 bmi.cpp
+++ ETS0916_15_mesud_melaku/cpp/Q1 bmi.cpp
@@ -1,15 +1,19 @@
 #include<iostream>
 #include<cmath>
- 
+
  using namespace std ;
  int main(){
-         double height,weight,bmi;
- 	cout<<"please, Enter your height in meter: ";
- 	cin>>height;
- 	cout<<"please, Enter your weight killogram: ";
- 	cin>> weight;
+
+    double height,weight,bmi;
+ 	cout << "please, Enter your height in meter: ";
+ 	cin >> height;
+
+ 	cout << "please, Enter your weight killograme: ";
+ 	cin >> weight;
  	bmi = (weight/(pow(height,2)));
- 	cout<<"Dear user your Body mass weight is "<<bmi <<" kg/m^2." ;
- 	
+
+ 	cout << "Dear user your Body mass weight is "<< bmi << " kg/m^2." ;
+
  	return 0 ;
- }
+ 	}
+
