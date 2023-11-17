30e4b1134f27a281ffae09064a92d46f5be1ae71
Mesud melaku <149295529+ellay21@users.noreply.github.com>
2023-11-17 15:11:52 +0300

    Update Q3 upperlowercase.cpp

--- ETS0916_15_mesud_melaku/cpp/Q3 upperlowercase.cpp
+++ ETS0916_15_mesud_melaku/cpp/Q3 upperlowercase.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 #include <ctype.h>
+
 using namespace std;
-int main()
-{
+
+int main(){
     char letter,upper,lower;
 
     cout << "Enter a letter: ";
     cin >> letter;
+
     upper = toupper(letter);
-    cout << "Uppercase equivalent: " << upper <<endl;
+    cout << "Uppercase equivalent: " << upper << endl;
     lower = tolower(letter);
-    cout << "Lowercase equivalent: " << lower <<endl;
+    cout << "Lowercase equivalent: " << lower << endl;
 
     return 0;
 }
+/* i can also use an if loop to make this program in a better way, i did not write using if loop
+because i thought the whole point of this question was to practice ctype library and not the loop. */
